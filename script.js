d93e66cf9abff6aa63e0d4d344214eb60894b9a0
Ricardo <ri.tapiasaez@gmail.com>
2023-08-05 18:45:40 -0400

    refactoring the code

--- script.js
+++ script.js
@@ -13,12 +13,17 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highScore = 0;
 
+const displayMessage = function (message) {
+  document.querySelector(".message").textContent = message;
+};
+
 document.querySelector(".again").addEventListener("click", function () {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
-  console.log("new number: " + secretNumber);
 
-  document.querySelector(".message").textContent = "  Start guessing...";
+  //   document.querySelector(".message").textContent = "  Start guessing...";
+  displayMessage("Start guessing...");
+
   document.querySelector(".score").textContent = score;
   document.querySelector(".number").textContent = "?";
   document.querySelector(".guess").value = "";
@@ -29,15 +34,16 @@ document.querySelector(".again").addEventListener("click", function () {
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
-  console.log(guess);
 
   // When there is no input
   if (!guess) {
-    document.querySelector(".message").textContent = "⛔ No number! ";
+    // document.querySelector(".message").textContent = "⛔ No number! ";
+    displayMessage("⛔ No number!");
 
     //When player wins
   } else if (guess === secretNumber) {
-    document.querySelector(".message").textContent = "🎊 Correct number!";
+    // document.querySelector(".message").textContent = "🎊 Correct number!";
+    displayMessage("🎊 Correct number!");
     document.querySelector(".number").textContent = secretNumber;
 
     document.querySelector("body").style.backgroundColor = "#60b347";
@@ -48,27 +54,40 @@ document.querySelector(".check").addEventListener("click", function () {
       document.querySelector(".highscore").textContent = highScore;
     }
 
-    //When guess is too high
-  } else if (guess > secretNumber) {
+    //When guess is wrong
+  } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector(".message").textContent = "📈 Too high!";
+      //   document.querySelector(".message").textContent =
+      //     guess > secretNumber ? "📈 Too high!" : "📉 Too low!";
+      displayMessage(guess > secretNumber ? "📈 Too high!" : "📉 Too low!");
       score--;
       document.querySelector(".score").textContent = score;
     } else {
-      document.querySelector(".message").textContent = "💥 You lost te game! ";
-      document.querySelector(".score").textContent = 0;
-    }
-
-    //When guess is too low
-  } else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector(".message").textContent = "📉 Too low!";
-      score--;
-      document.querySelector(".score").textContent = score;
-    } else {
-      document.querySelector(".message").textContent = "💥 You lost te game! ";
+      //   document.querySelector(".message").textContent = "💥 You lost te game!";
+      displayMessage("💥 You lost te game!");
       document.querySelector(".score").textContent = 0;
     }
   }
+  //     //When guess is too high
+  //   } else if (guess > secretNumber) {
+  //     if (score > 1) {
+  //       document.querySelector(".message").textContent = "📈 Too high!";
+  //       score--;
+  //       document.querySelector(".score").textContent = score;
+  //     } else {
+  //       document.querySelector(".message").textContent = "💥 You lost te game! ";
+  //       document.querySelector(".score").textContent = 0;
+  //     }
+
+  //     //When guess is too low
+  //   } else if (guess < secretNumber) {
+  //     if (score > 1) {
+  //       document.querySelector(".message").textContent = "📉 Too low!";
+  //       score--;
+  //       document.querySelector(".score").textContent = score;
+  //     } else {
+  //       document.querySelector(".message").textContent = "💥 You lost te game! ";
+  //       document.querySelector(".score").textContent = 0;
+  //     }
+  //   }
 });
-console.log(secretNumber);
