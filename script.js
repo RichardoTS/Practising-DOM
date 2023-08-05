19b1f6310ca3f4fec9d1b0d266cf4f33e4d4ee3e
Ricardo <ri.tapiasaez@gmail.com>
2023-08-05 14:31:05 -0400

    implementing game logic

--- script.js
+++ script.js
@@ -1,11 +1,44 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
-
+/*console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = 'Correct number! 🎊';
 
 console.log(document.querySelector('.number').textContent);
 console.log(document.querySelector('.score').textContent);
 
 document.querySelector('.guess').value = 17;
-console.log(document.querySelector('.guess').value);
+console.log(document.querySelector('.guess').value);*/
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No number! ';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎊 Correct number!';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost te game! ';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost te game! ';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
