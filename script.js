4b7b66fd55297b862b92d4ef2e6ae924585bedad
Ricardo <ri.tapiasaez@gmail.com>
2023-08-05 01:34:24 -0400

    more learning

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 'use strict';
 
 console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = 'Correct number! 🎊';
+
+console.log(document.querySelector('.number').textContent);
+console.log(document.querySelector('.score').textContent);
+
+document.querySelector('.guess').value = 17;
+console.log(document.querySelector('.guess').value);
